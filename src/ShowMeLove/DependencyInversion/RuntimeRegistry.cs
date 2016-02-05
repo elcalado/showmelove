d05c9f9175ad695388100db147d26d9eb9a20896
pedias <pedias@microsoft.com>
2016-02-05 14:10:52 -0800

    Added dependencies to the UWP app

---
+++ src/ShowMeLove/DependencyInversion/RuntimeRegistry.cs
@@ -0,0 +1,27 @@
+﻿using ShowMeLove.Business.Managers;
+using ShowMeLove.Data.Identity;
+using StructureMap;
+
+namespace ShowMeLove.DependencyInversion
+{
+    public class RuntimeRegistry : Registry
+    {
+        public RuntimeRegistry()
+        {
+            Scan(x =>
+            {
+
+                // From the business layer
+                x.Assembly("ShowMeLove.Business.Managers");
+
+                // From the data layer
+                x.Assembly("ShowMeLove.Data.AzureStorage");
+                x.Assembly("ShowMeLove.Data.EventHub");
+                x.Assembly("ShowMeLove.Data.Identity");
+                x.Assembly("ShowMeLove.Data.Fakes");
+
+                x.WithDefaultConventions();
+            });
+        }
+    }
+}
