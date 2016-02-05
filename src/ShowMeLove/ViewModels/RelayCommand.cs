6f5c216b7723b0a08c441c43a719dcfa3a3e493f
pedias <pedias@microsoft.com>
2016-02-05 15:40:22 -0800

    Added a RelayCommand for all XAML buttons

---
+++ src/ShowMeLove/ViewModels/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ShowMeLove.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+        private Func<bool> _canExecuteFunction;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action action, Func<bool> canExecuteFunction = null)
+        {
+            _action = action;
+            _canExecuteFunction = canExecuteFunction;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteFunction != null)
+                return _canExecuteFunction.Invoke();
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.invoke();
+        }
+    }
+}
