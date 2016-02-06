0f90952e618a2e9835bab6915c087cad511a9d96
luiscalado <elcalado2010@hotmail.com>
2016-02-06 07:19:19 -0800

    Web Api In

---
+++ src/SmlApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmlApi.MvcApplication" Language="C#" %>
---
+++ src/SmlApi/Models/Session.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace SmlApi.Models
+{
+    public class Session
+    {
+        public int SessionId { get; set; }
+        public string Title { get; set; }
+        public string Self
+        {
+            get
+            {
+                return string.Format(CultureInfo.CurrentCulture,
+               "api/sessions/{0}", this.SessionId);
+            }
+            set { }
+        }
+    }
+}
