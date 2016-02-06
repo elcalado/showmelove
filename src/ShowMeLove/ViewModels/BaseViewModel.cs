b5b88645732c7eddb0696f893ebd3d34407cd71e
pedias <pedias@microsoft.com>
2016-02-05 16:12:23 -0800

    Started working on the layout of the app, applied a viewmodel

---
+++ src/ShowMeLove/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace ShowMeLove.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void ShoutAbout(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                return;
+
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/ShowMeLove/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,92 @@
+﻿using ShowMeLove.Domain.Core.Contracts.Managers;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ShowMeLove.ViewModels
+{
+    internal class MainPageViewModel : BaseViewModel
+    {
+        private readonly IImageManager _imageManager;
+
+        // Visible assets, bindables
+        private string _userName;
+        private string _timeLeft;
+        private BitmapImage _lastImage;
+        private RelayCommand _pauseCommand;
+
+        public MainPageViewModel(IImageManager imageManager)
+        {
+            _imageManager = imageManager;
+        }
+
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                if (_userName == value)
+                    return;
+
+                _userName = value;
+                ShoutAbout("UserName");
+            }
+        }
+
+
+        public string TimeLeft
+        {
+            get { return _timeLeft; }
+            set
+            {
+                if (_timeLeft == value)
+                    return;
+
+                _timeLeft = value;
+                ShoutAbout("TimeLeft");
+            }
+        }
+
+
+        public BitmapImage LastImage
+        {
+            get { return _lastImage; }
+            set
+            {
+                if (_lastImage == value)
+                    return;
+
+                _lastImage = value;
+                ShoutAbout("LastImage");
+            }
+        }
+
+        public ICommand PauseCommand
+        {
+            get
+            {
+                if (_pauseCommand == null)
+                    _pauseCommand = new RelayCommand(DoPause);
+
+                return _pauseCommand;
+            }
+        }
+
+
+        private void DoPause()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task InitializeAsync()
+        {
+            var result = await _imageManager.InitializeAsync();
+
+            if (!result)
+                throw new InvalidProgramException("Failed to initialize. oh crap!");
+        }
+    }
+}
