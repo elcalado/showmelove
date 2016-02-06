8e1ae122df364a0d0e2a1c32271f58021c489f07
luiscalado <elcalado2010@hotmail.com>
2016-02-06 07:34:57 -0800

    added support for entity model

---
+++ src/SmlApi/Migrations/Configuration.cs
@@ -0,0 +1,43 @@
+namespace SmlApi.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using SmlApi.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SmlApi.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SmlApi.Models.ApplicationDbContext context)
+        {
+            context.Sessions.AddOrUpdate(p => p.SessionId,
+               new Session
+               {
+                   Title = "Azure Active Directory Unplugged",
+               },
+                new Session
+                {
+                    Title = "The Future of Azure Compute",
+                },
+                new Session
+                {
+                    Title = "Developing for Microsoft Band",
+                },
+                new Session
+                {
+                    Title = "Creating Powerful dashboards with PowerBI",
+                },
+                new Session
+                {
+                    Title = "Going Cross Platform with Cordova",
+                }
+                );
+        }
+    }
+    }
+}
