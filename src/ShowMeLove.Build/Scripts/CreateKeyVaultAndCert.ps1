e42ac53c29181fdf97318fe83df168c4ec8d6508
marrobi <marrobi@microsoft.com>
2016-02-05 17:21:41 -0800

    Add KeyVault Script

---
+++ src/ShowMeLove.Build/Scripts/CreateKeyVaultAndCert.ps1
@@ -0,0 +1,50 @@
+#Requires -Module AzureRM.KeyVault
+
+# Use this script to create a certificate that you can use to secure a Service Fabric Cluster
+# This script requires an existing KeyVault that is EnabledForDeployment.  The vault must be in the same region as the cluster.
+# To create a new vault and set the EnabledForDeployment property run:
+#
+# New-AzureRmResourceGroup -Name KeyVaults -Location WestUS
+# New-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName KeyVaults -Location WestUS -EnabledForDeployment
+#
+# Once the certificate is created and stored in the vault, the script will provide the parameter values needed for template deployment
+# 
+
+param(
+    [string] [Parameter(Mandatory=$true)] $Password,
+    [string] [Parameter(Mandatory=$true)] $CertDNSName,
+    [string] [Parameter(Mandatory=$true)] $KeyVaultName,
+    [string] [Parameter(Mandatory=$true)] $KeyVaultSecretName,
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+      [string] [Parameter(Mandatory=$true)] $Location
+)
+
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+New-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -Location WestUS -EnabledForDeployment
+
+$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+$CertFileFullPath = $(Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Definition) "\$CertDNSName.pfx")
+
+$NewCert = New-SelfSignedCertificate -CertStoreLocation Cert:\CurrentUser\My -DnsName $CertDNSName 
+Export-PfxCertificate -FilePath $CertFileFullPath -Password $SecurePassword -Cert $NewCert
+
+$Bytes = [System.IO.File]::ReadAllBytes($CertFileFullPath)
+$Base64 = [System.Convert]::ToBase64String($Bytes)
+
+$JSONBlob = @{
+    data = $Base64
+    dataType = 'pfx'
+    password = $Password
+} | ConvertTo-Json
+
+$ContentBytes = [System.Text.Encoding]::UTF8.GetBytes($JSONBlob)
+$Content = [System.Convert]::ToBase64String($ContentBytes)
+
+$SecretValue = ConvertTo-SecureString -String $Content -AsPlainText -Force
+$NewSecret = Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultSecretName -SecretValue $SecretValue -Verbose
+
+Write-Host
+Write-Host "Resource Id: "$(Get-AzureRmKeyVault -VaultName $KeyVaultName).ResourceId
+Write-Host "Secret URL : "$NewSecret.Id
+Write-Host "Thumbprint : "$NewCert.Thumbprint
+
