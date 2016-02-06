69f78a811ee5560af814c6a34ee4af134a503404
luiscalado <elcalado2010@hotmail.com>
2016-02-06 09:27:50 -0800

    new session api availalble

---
+++ src/SmlApi/Controllers/SessionsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SmlApi.Models;
+
+namespace SmlApi.Controllers
+{
+    public class SessionsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Sessions
+        public IQueryable<Session> GetSessions()
+        {
+            return db.Sessions;
+        }
+
+        // GET: api/Sessions/5
+        [ResponseType(typeof(Session))]
+        public IHttpActionResult GetSession(int id)
+        {
+            Session session = db.Sessions.Find(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(session);
+        }
+
+        // PUT: api/Sessions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSession(int id, Session session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != session.SessionId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(session).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Sessions
+        [ResponseType(typeof(Session))]
+        public IHttpActionResult PostSession(Session session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Sessions.Add(session);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = session.SessionId }, session);
+        }
+
+        // DELETE: api/Sessions/5
+        [ResponseType(typeof(Session))]
+        public IHttpActionResult DeleteSession(int id)
+        {
+            Session session = db.Sessions.Find(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            db.Sessions.Remove(session);
+            db.SaveChanges();
+
+            return Ok(session);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SessionExists(int id)
+        {
+            return db.Sessions.Count(e => e.SessionId == id) > 0;
+        }
+    }
+}
